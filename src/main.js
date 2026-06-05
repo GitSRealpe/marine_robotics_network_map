c87fe9cbcdc98b98d69a0a655ec22db10e0f5ccc
GitSRealpe <msn-realpe@hotmail.com>
2026-06-05 21:42:43 +0200

    using tailwindcss now

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
-import 'bootstrap';  // Bootstrap JS (optional, needed for modals, dropdowns, etc.)
-
 const map = new maplibregl.Map({
 	container: 'map',
 	style: 'map_style.json', // reliable free style
@@ -60,22 +57,60 @@ map.on('load', async () => {
 			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
 		}
 
-		new maplibregl.Popup()
+		let div = document.createElement('div');
+		div.innerHTML = `<div class="bg-white border border-gray-800 rounded-xl overflow-hidden">
+	<div class="px-0 py-0 border-b border-gray-200 flex items-center justify-between">
+		<div class="flex items-center gap-3">
+			<div>
+				<h3 class="px-4 py-2 bg-gray-300 border-b border-gray-200 text-sm font-bold text-gray-500">${properties.location}</h3>
+				<div class="px-4 py-3">
+					<h3 class="text-2xl font-bold text-shadow-lg/30 text-sky-800">${properties.name}</h3>
+					<h6 class="text-lg text-gray-700">${properties.subtitle}</h6>
+					<p class="text-base text-gray-500">${properties.affiliation}</p>
+				</div>
+			</div>
+		</div>
+		</div>
+		<div class="px-5 pb-2 pt-0 flex justify-center">
+			<a href="${properties.homepage}" class="text-sm px-3 py-1 rounded-lg border border-blue-400 text-gray-500 hover:bg-gray-300">${properties.homepage}</a>
+		</div>
+		</div > `;
+
+		new maplibregl.Popup({ maxWidth: 'none' })
 			.setLngLat(coordinates)
-			.setHTML(`<div class="card-header">${properties.location}</div>
-    						<div class="card-body">
-      					<h5 class="card-title">${properties.name}</h5>
-      					<h6 class="card-subtitle mb-2 text-muted">${properties.subtitle}</h6>
-					      <p>${properties.affiliation}</p>
-      					<div class="text-center">
-            		<a href="${properties.homepage}" class="btn btn-outline-primary" target="_blank">${properties.homepage}</a>
-        				</div>
-						    </div>`)
+			.setDOMContent(div)
 			.addTo(map);
 	});
 
 
 });
+
+// for designing cards
+// let properties = { location: "Girona, Spain", name: "CIRS", subtitle: "Centre d'Investigacio en Robotica Submarina", affiliation: "Universitat de Girona", homepage: "https://cirs.udg.edu/" }
+
+// let div = document.createElement('div');
+// div.innerHTML = `<div class="bg-white border border-gray-800 rounded-xl overflow-hidden">
+// 	<div class="px-0 py-0 border-b border-gray-200 flex items-center justify-between">
+// 		<div class="flex items-center gap-3">
+// 			<div>
+// 				<h3 class="px-4 py-2 bg-gray-300 border-b border-gray-200 text-sm font-bold text-gray-500">${properties.location}</h3>
+// 				<div class="px-4 py-3">
+// 					<h3 class="text-2xl font-bold text-shadow-lg/30 text-sky-800">${properties.name}</h3>
+// 					<h6 class="text-lg text-gray-700">${properties.subtitle}</h6>
+// 					<p class="text-base text-gray-500">${properties.affiliation}</p>
+// 				</div>
+// 			</div>
+// 		</div>
+// 		</div>
+// 		<div class="px-5 pb-2 pt-0 flex justify-center">
+// 			<a href="${properties.homepage}" class="text-sm px-3 py-1 rounded-lg border border-blue-400 text-gray-500 hover:bg-gray-300">${properties.homepage}</a>
+// 		</div>
+// 		</div > `;
+
+// new maplibregl.Popup()
+// 	.setDOMContent(div)
+// 	.setLngLat([-0.07408400365127464, 39.99450141565572])
+// 	.addTo(map);
 
 // Change the cursor to a pointer when the mouse is over the places layer.
 map.on('mouseenter', 'places', () => {
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
     base: '/marine_robotics_network_map/', // Important for GitHub Pages to load assets correctly
@@ -10,4 +11,7 @@ export default defineConfig({
         port: 3000,
         open: false, // Automatically open browser
     },
+    plugins: [
+        tailwindcss(),
+    ],
 });
