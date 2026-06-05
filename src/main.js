6172984c1700768a3e665623fcfe98b49c4b9b05
GitSRealpe <msn-realpe@hotmail.com>
2026-06-05 18:18:18 +0200

    using maplibregl js now

--- src/main.js
+++ src/main.js
@@ -1,106 +1,91 @@
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 import 'bootstrap/dist/css/bootstrap.min.css';  // Bootstrap CSS
 import 'bootstrap';  // Bootstrap JS (optional, needed for modals, dropdowns, etc.)
 
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
-import Stats from 'stats.js';
-import GUI from 'lil-gui';
-// import ThreeGlobe from 'three-globe';
-
-const gui = new GUI();
-gui.add(document, 'title');
-
-import { buildGlobe } from './myGlobe.js'
-
-// Setup renderers
-const renderers = [new THREE.WebGLRenderer(), new CSS2DRenderer()];
-renderers.forEach((r, idx) => {
-    r.setSize(window.innerWidth, window.innerHeight);
-    if (idx > 0) {
-        // overlay additional on top of main renderer
-        r.domElement.style.position = 'absolute';
-        r.domElement.style.top = '0px';
-        r.domElement.style.pointerEvents = 'none';
-    }
-    document.getElementById('globeViz').appendChild(r.domElement);
+const map = new maplibregl.Map({
+	container: 'map',
+	style: 'map_style.json', // reliable free style
+	center: [2.8363417576785377, 41.967662037896005],
+	zoom: 3
 });
 
-// Setup scene
-const scene = new THREE.Scene();
-scene.add(new THREE.AmbientLight(0xcccccc, 3));
-const dirlight = new THREE.DirectionalLight(0xffffff, 3)
-dirlight.position.set(1, 1, 1)
-scene.add(dirlight);
+map.on('style.load', () => {
+	map.setProjection({
+		type: 'globe', // Set projection to globe
+	});
+});
 
-// Setup camera
-const camera = new THREE.PerspectiveCamera();
-camera.aspect = window.innerWidth / window.innerHeight;
-camera.updateProjectionMatrix();
-camera.position.z = 300;
 
-// Orbit Controls
-const controls = new OrbitControls(camera, renderers[0].domElement);
-controls.enablePan = false
-controls.enableDamping = true
-controls.autoRotate = true
-controls.autoRotateSpeed = 0.1
-controls.minDistance = 130
-controls.maxDistance = 300
+map.on('load', async () => {
+	const image = await map.loadImage('https://maplibre.org/maplibre-gl-js/docs/assets/custom_marker.png');
+	// Add an image to use as a custom marker
+	map.addImage('custom-marker', image.data);
 
-// Fetch JSON data
-fetch('markers.json')  // The URL is relative to the "public" folder
-    .then(response => response.json())
-    .then(data => {
-        const gData = [];
-        data.markers.forEach(place => {
-            gData.push({
-                lat: place.lat,
-                lng: place.long,
-                name: place.name,
-                subtitle: place.subtitle,
-                location: place.location,
-                affiliation: place.affiliation,
-                // description: place.description,
-                homepage: place.homepage,
-                size: 25,
-                color: ['red', 'purple', 'blue', 'green', 'gold'][Math.round(Math.random() * 4)],
+	// add marker list
+	map.addSource('places', { type: 'geojson', data: 'markers.json' });
 
-            })
-        });
-        let globe = buildGlobe(gData)
-        scene.add(globe)
-        // Update pov when camera moves
-        globe.setPointOfView(camera);
-        controls.addEventListener('change', () => globe.setPointOfView(camera));
+	// Add a layer showing the places.
+	map.addLayer({
+		id: 'places',
+		type: 'symbol',
+		source: 'places',
+		layout: {
+			'icon-image': '{icon}',
+			'icon-overlap': 'always',
+			'icon-size': 2,
+			'text-field': ['get', 'name'],
+			'text-offset': [0, 1.25],
+			'text-anchor': 'top'
+		},
+		paint: {
+			'text-color': '#000000',
+			'text-halo-color': '#ffffff',
+			'text-halo-width': 2
+		},
+	});
 
-    })
-    .catch(error => {
-        console.error('Error loading JSON file:', error);
-    });
+	// When a click event occurs on a feature in the places layer, open a popup at the
+	// location of the feature, with description HTML from its properties.
+	map.on('click', 'places', (e) => {
+		const coordinates = e.features[0].geometry.coordinates.slice();
+		const description = e.features[0].properties.description;
+		const properties = e.features[0].properties;
 
-const texture = new THREE.TextureLoader().load("https://cdn.jsdelivr.net/npm/three-globe/example/img/night-sky.png");
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(1, 1);
+		// Ensure that if the map is zoomed out such that multiple
+		// copies of the feature are visible, the popup appears
+		// over the copy being pointed to.
+		while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+			coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+		}
 
-const geometry = new THREE.SphereGeometry(500, 32, 16);
-// const material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.BackSide });
-const material = new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+		new maplibregl.Popup()
+			.setLngLat(coordinates)
+			.setHTML(`<div class="card-header">${properties.location}</div>
+    						<div class="card-body">
+      					<h5 class="card-title">${properties.name}</h5>
+      					<h6 class="card-subtitle mb-2 text-muted">${properties.subtitle}</h6>
+					      <p>${properties.affiliation}</p>
+      					<div class="text-center">
+            		<a href="${properties.homepage}" class="btn btn-outline-primary" target="_blank">${properties.homepage}</a>
+        				</div>
+						    </div>`)
+			.addTo(map);
+	});
 
-var stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom);
 
-// Kick-off renderers
-(function animate() { // IIFE
-    stats.begin();
-    // Frame cycle
-    controls.update();
-    sphere.rotation.y -= 0.0002;
-    renderers.forEach(r => r.render(scene, camera));
-    requestAnimationFrame(animate);
-    stats.end();
-})();
+});
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+map.on('mouseenter', 'places', () => {
+	map.getCanvas().style.cursor = 'pointer';
+});
+
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'places', () => {
+	map.getCanvas().style.cursor = '';
+});
+
+
+map.addControl(new maplibregl.FullscreenControl());
